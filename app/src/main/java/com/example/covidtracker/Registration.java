45eeffe4c56914a9ca1da4b6130a41b9405ea6f6
Minh <Minhmovies99@gmail.com>
2020-04-01 13:08:39 +0700

    Verify_Test

--- app/src/main/java/com/example/covidtracker/Registration.java
+++ app/src/main/java/com/example/covidtracker/Registration.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -22,6 +24,16 @@ public class Registration extends AppCompatActivity {
     private EditText fullname_input, email_input, password_input, password_confirm;
     private Button registration_button;
     private FirebaseAuth authentication;
+    private boolean verify = false;
+    private static final String TAG = "Registration";
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,12 @@ public class Registration extends AppCompatActivity {
         authentication = FirebaseAuth.getInstance();
 
         registration_button.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+
+
             @Override
             public void onClick(View v) {
                 final String email = email_input.getText().toString();
@@ -53,24 +71,52 @@ public class Registration extends AppCompatActivity {
                 }else {
 
 
-                    authentication.createUserWithEmailAndPassword(email, password).addOnCompleteListener(Registration.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (!task.isSuccessful()) {
 
-                                Toast.makeText(Registration.this, "Sign up Error", Toast.LENGTH_SHORT).show();
 
-                            } else {
-                                String user_id = authentication.getCurrentUser().getUid();
-                                DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child(fullname).child("User_ID").child(user_id);
-                                current_user_db.setValue(true);
-                                Toast.makeText(Registration.this, "Registration Successfully", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(Registration.this, Login.class);
-                                startActivity(intent);
+
+                        authentication.createUserWithEmailAndPassword(email, password).addOnCompleteListener(Registration.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+
+                                    Toast.makeText(Registration.this, "Sign up Error", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    authentication.getCurrentUser().sendEmailVerification()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    // Re-enable button
+                                                    findViewById(R.id.registration_button).setEnabled(true);
+
+                                                    if (task.isSuccessful()) {
+                                                        Toast.makeText(Registration.this,
+                                                                "Verification email sent to " + email_input.getText().toString(),
+                                                                Toast.LENGTH_SHORT).show();
+                                                        verify = true;
+                                                    } else {
+                                                        Log.e(TAG, "sendEmailVerification", task.getException());
+                                                        Toast.makeText(Registration.this,
+                                                                "Failed to send verification email.",
+                                                                Toast.LENGTH_SHORT).show();
+
+                                                        verify = false;
+                                                    }
+                                                }
+                                            });
+
+                                    String user_id = authentication.getCurrentUser().getUid();
+                                    findViewById(R.id.registration_button).setEnabled(true);
+                                    DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child(fullname).child("User_ID").child(user_id);
+                                    current_user_db.setValue(true);
+
+                                    Toast.makeText(Registration.this, "Registration Successfully", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(Registration.this, Login.class);
+                                    startActivity(intent);
+                                    }
 
                             }
-                        }
-                    });
+                        });
 
                 }
 
